43b7ea4cce2b17eefec42d2de2142dc41eb8c5b7
RenatoAF <renato012br@hotmail.com>
2023-10-18 21:21:56 -0300

    fix: urlbase

--- src/utilitarios/definiçoes.js
+++ src/utilitarios/definiçoes.js
@@ -1,8 +1,8 @@
-//export const urlBase = "https://129.146.68.51/aluno15-pfsii/visitantes";
-//export const urlBase2 = "https://129.146.68.51/aluno15-pfsii/agendamento";
-//export const urlBase3 = "https://129.146.68.51/aluno15-pfsii/categorias";
+export const urlBase = "https://129.146.68.51/aluno38-pfsii/professor";
+export const urlBase2 = "https://129.146.68.51/aluno38-pfsii/turma";
+export const urlBase3 = "https://129.146.68.51/aluno38-pfsii/curso";
 
 
-export const urlBase = "http://localhost:3001/professor";
+/*export const urlBase = "http://localhost:3001/professor";
 export const urlBase2 = "http://localhost:3001/turma";
-export const urlBase3 = "http://localhost:3001/curso";
+export const urlBase3 = "http://localhost:3001/curso";*/
